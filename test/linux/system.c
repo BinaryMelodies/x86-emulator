e81a9d261705ece247891d37f32366a9c9c1a4df
Binary Melodies <192211378+BinaryMelodies@users.noreply.github.com>
2025-10-05 21:48:58 +0200

    Add Linux ABI test

---
+++ test/linux/system.c
@@ -0,0 +1,103 @@
+
+#include "system.h"
+
+asm(
+	".global\t_start\n"
+	"_start:\n"
+#if __ia16__
+	"\tmovw\t%sp, %bp\n"
+	"\tmovw\t(%bp), %ax\n"
+	"\tleaw\t2(%bp), %bx\n"
+	"\tmovw\t%ax, %si\n"
+	"\tshlw\t$1, %si\n"
+	"\tleaw\t4(%bp,%si), %si\n"
+	"\tpushw\t%si\n"
+	"\tpushw\t%bx\n"
+	"\tpushw\t%ax\n"
+#elif __i386__
+	"\tmovl\t(%esp), %eax\n"
+	"\tleal\t4(%esp), %ebx\n"
+	"\tleal\t8(%esp,%eax,4), %ecx\n"
+	"\tpushl\t%ecx\n"
+	"\tpushl\t%ebx\n"
+	"\tpushl\t%eax\n"
+#elif __amd64__
+	"\tmovq\t(%rsp), %rdi\n"
+	"\tleaq\t8(%rsp), %rsi\n"
+	"\tleaq\t16(%rsp,%rdi,8), %rdx\n"
+#else
+# error
+#endif
+	"\tcall\tmain\n"
+#if __ia16__
+	"\tpushw\t%ax\n"
+#elif __i386__
+	"\tpushl\t%eax\n"
+#elif __amd64__
+	"\tmovq\t%rax, %rdi\n"
+#else
+# error
+#endif
+	"\tcall\texit\n"
+);
+
+noreturn void exit(int status)
+{
+# if __ia16__ || __i386__
+	asm("int\t$0x80"
+		:
+		: "a"(1), "b"(status));
+# elif __amd64__
+	asm("syscall"
+		:
+		: "a"(60), "D"(status));
+# else
+#  error
+# endif
+	for(;;)
+		;
+}
+
+ssize_t write(int fd, const void * buf, size_t count)
+{
+	ssize_t result;
+# if __ia16__ || __i386__
+	asm(
+		"int\t$0x80"
+			: "=a"(result)
+			: "a"(4), "b"(fd), "c"(buf), "d"(count));
+# elif __amd64__
+	asm("syscall"
+			: "=a"(result)
+			: "a"(1), "D"(fd), "S"(buf), "d"(count));
+# else
+#  error
+# endif
+	return result;
+}
+
+size_t strlen(const char * s)
+{
+	size_t length;
+	for(length = 0; s[length] != '\0'; length++)
+		;
+	return length;
+}
+
+void putstr(const char * s)
+{
+	write(1, s, strlen(s));
+}
+
+void putint(unsigned int value)
+{
+	char buffer[(sizeof(int) * 5 + 1) / 2];
+	int pointer = sizeof buffer;
+	do
+	{
+		buffer[--pointer] = '0' + (value % 10);
+		value /= 10;
+	} while(value != 0);
+	write(1, &buffer[pointer], sizeof buffer - pointer);
+}
+
---
+++ test/linux/system.h
@@ -0,0 +1,47 @@
+#ifndef _SYSTEM_H
+#define _SYSTEM_H
+
+#define noreturn _Noreturn
+
+typedef _Bool bool;
+#define false ((bool)0)
+#define true  ((bool)1)
+
+#if __ia16__
+# define far __far
+#else
+# define far
+#endif
+
+typedef   signed char   int8_t;
+typedef unsigned char  uint8_t;
+typedef   signed short  int16_t;
+typedef unsigned short uint16_t;
+
+#if __i386__ || __amd64__
+typedef   signed int        int32_t;
+typedef unsigned int       uint32_t;
+#else
+typedef   signed long       int32_t;
+typedef unsigned long      uint32_t;
+#endif
+
+#if __amd64__
+typedef   signed long       int64_t;
+typedef unsigned long      uint64_t;
+typedef   signed long       ssize_t;
+typedef unsigned long       size_t;
+#else
+typedef   signed long long  int64_t;
+typedef unsigned long long uint64_t;
+typedef   signed int       ssize_t;
+typedef unsigned int        size_t;
+#endif
+
+noreturn void exit(int status);
+ssize_t write(int fd, const void * buf, size_t count);
+size_t strlen(const char * s);
+void putstr(const char * s);
+void putint(unsigned int value);
+
+#endif /* _SYSTEM_H */
